5327ec277a599c2ad1dd7d088b33abdd0d70aa89
Bei_ko <84181031+xieweiling@users.noreply.github.com>
2022-12-23 11:29:17 +0800

    Add files via upload

---
+++ assets/js/loading.js
@@ -0,0 +1,23 @@
+//加载完成后执行
+window.addEventListener('load', function () {
+
+    //载入动画
+    $('#loading-box').attr('class', 'loaded');
+    $('#bg').css("cssText", "transform: scale(1);filter: blur(0px);transition: ease 1.5s;");
+    $('.cover').css("cssText", "opacity: 1;transition: ease 1.5s;");
+    $('#section').css("cssText", "transform: scale(1) !important;opacity: 1 !important;filter: blur(0px) !important");
+
+    //中文字体缓加载-此处写入字体源文件
+    //先行加载简体中文子集，后续补全字集
+    //由于压缩过后的中文字体仍旧过大，可转移至对象存储或 CDN 加载
+    const font = new FontFace(
+        "MiSans",
+        "url(" + "./font/MiSans-Regular.woff2" + ")"
+    );
+    document.fonts.add(font);
+
+}, false)
+
+setTimeout(function () {
+    $('#loading-text').html("加载需要一定时间...")
+}, 3000);
